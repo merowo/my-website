1d6849f629d43d309ff5a05ebf0415809559457a
mstrv <furry@mstrv.pl>
2024-04-02 23:37:02 +0200

    hotpatch, idk what i changed

--- discord/script.js
+++ discord/script.js
@@ -1,4 +1,2 @@
 //waits for 3 seconds and redirects to dsc.gg/mstrv
-setTimeout(function() {
-    window.location.href = "https://dsc.gg/mstrv";
-}, 3000);
+window.location.href('https://dsc.gg/mstrv');
